c9671494c48f2913caf7ca10ab920bcc895d5a56
nolantoshack <nolan.toshack@ontariotechu.net>
2025-12-05 19:25:46 -0500

    updated url

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -41,7 +41,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'https://truck-shipment-managment.onrender.com/auth/google/callback'
+        callbackURL: 'https://infr3120-fall25-project-5e00.onrender.com/auth/google/callback'
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
