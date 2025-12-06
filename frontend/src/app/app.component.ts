e0d8b4205b253d46d227ac108d9c6dcfa4f758be
nolantoshack <nolan.toshack@ontariotechu.net>
2025-12-05 19:56:16 -0500

    fixed logo, passport url and other errors

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,15 @@
-// src/app/app.component.ts
-
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule, 
-    RouterOutlet // Required to render nested components
+    RouterOutlet, // Required to render nested components
+    HttpClientModule
   ],
   templateUrl: './app.component.html',
 })
